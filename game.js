5e2617d91352ee27b3012df330b34a22e2c76cd0
Tofer-Cich <[tofercich@hotmail.com]>
2024-08-29 15:43:50 -0500

    Start designing basic implementation
    some general outline ideas in game.js for how to design the game and
    player turns.

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
 console.log("welcome");
+
+//Objects: Gameboard, players, gameFlow.
+
+//Use IIFE's for gameboard and other elements that only need a single instance
+
+//Factory functions for players
+// Player gets a turn function where they decide where to put their mark
+
+//store the gameboard as an array inside gameboard IIFE
+

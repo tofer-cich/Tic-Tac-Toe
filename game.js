54fc1bd6db448a671af75727176560128bbd538a
Tofer-Cich <[tofercich@hotmail.com]>
2024-09-02 12:27:29 -0500

    Implement winning conditions and declare winner
    Game logic allows game to be played and either won or end in a draw.
    Next steps are HTML implementation for the view.

--- game.js
+++ game.js
@@ -1,12 +1,3 @@
-//Objects: Gameboard, players, gameFlow.
-
-//Use IIFE's for gameboard and other elements that only need a single instance
-
-//Factory functions for players
-// Player gets a turn function where they decide where to put their mark
-
-//store the gameboard as an array inside gameboard IIFE
-
 const board = (function () {
     let gameBoard = [];
     const rows = 3;
@@ -19,8 +10,24 @@ const board = (function () {
         }
     }
     const addMark = (row, column, player) => {
-        if (gameBoard[row][column] != 0) return;
-        gameBoard[row][column] = player;
+        if ((row >= rows || column >= columns) || gameBoard[row][column].getValue() != 0) {
+            return -1;
+        } else {
+            gameBoard[row][column].addMarkValue(player);
+            return player;
+        }
+    }
+
+    const getSpacesLeft = () => {
+        let numLeft = rows * columns;
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                if (gameBoard[i][j].getValue() != 0) {
+                    numLeft--;
+                }
+            }
+        }
+        return numLeft;
     }
 
     const printBoard = () => {
@@ -28,23 +35,23 @@ const board = (function () {
         console.log(boardWithValues);
     }
 
-    return {gameBoard, addMark, printBoard};
+    return { gameBoard, addMark, getSpacesLeft, printBoard };
 })();
 
 function createPlayer(name) {
-    return {name};
+    return { name };
 }
 
 function cell() {
     let value = 0;
 
-    const addMark = (player) => {
+    const addMarkValue = (player) => {
         value = player;
     };
 
     const getValue = () => value;
 
-    return {addMark, getValue};
+    return { addMarkValue, getValue };
 }
 
 function gameController(playerOne = "Player One", playerTwo = "Player Two") {
@@ -52,11 +59,13 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
     const players = [
         {
             name: playerOne,
-            token: 1
+            token: 1,
+            moves: []
         },
         {
-            name:playerTwo,
-            token:2
+            name: playerTwo,
+            token: 2,
+            moves: []
         }
     ];
 
@@ -72,18 +81,63 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
         board.printBoard();
     }
 
+    const checkWinner = (moves) => {
+        const winningCombinations = [
+            [[0, 0], [0, 1], [0, 2]],
+            [[1, 0], [1, 1], [1, 2]],
+            [[2, 0], [2, 1], [2, 2]],
+            [[0, 0], [1, 0], [2, 0]],
+            [[0, 1], [1, 1], [2, 1]],
+            [[0, 2], [1, 2], [2, 2]],
+            [[0, 0], [1, 1], [2, 2]],
+            [[0, 2], [1, 1], [2, 0]]
+        ];
+
+        const movesSet = new Set(moves.map(move => move.toString()));
+
+        return winningCombinations.some(combination => combination.every(([row, column]) => movesSet.has([row, column].toString())));
+    }
+
     const playRound = (row, column) => {
-        board.addMark(row, column, getActivePlayer().token);
+        let validMove = board.addMark(row, column, getActivePlayer().token);
+        const currentPlayerToken = getActivePlayer().token;
 
-        //Check winning conditions
+        if (validMove != -1) {
+            getActivePlayer().moves.push([row, column]);
 
-        switchPlayerTurn();
+            if (checkWinner(getActivePlayer().moves)) {
+                winner = getActivePlayer().name;
+            } else if (board.getSpacesLeft() <= 0) {
+                draw = !draw;
+            }
+
+            switchPlayerTurn();
+        } else {
+            console.log("Can't play there");
+        }
+
         printNewRound();
     };
 
     printNewRound();
 
-    return {playRound, getActivePlayer};
+    let winner;
+    let draw = false;
+
+    while (!winner && !draw) {
+        let playerRow = prompt("Choose a row");
+        let playerColumn = prompt("Choose a column");
+
+        playRound(playerRow, playerColumn);
+    }
+
+    if (draw) {
+        console.log("Look's like a draw");
+    } else {
+        console.log(winner + " won the game!");
+    }
+
+    return { playRound, getActivePlayer, checkWinner };
 }
 
 const game = gameController();
